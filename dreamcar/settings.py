add8eaf3536cc44f365eca955f8d8b28734655c1
Sultan Alnuaimi <alnuaimi19@gmail.com>
2021-01-06 15:48:25 +0400

    adding new model

--- dreamcar/settings.py
+++ dreamcar/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'dreamcar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dreamer_db',
+        'USER': 'postgres',
+        'PASSWORD': 'secret',
+        'HOST': 'localhost',
     }
 }
 
@@ -124,5 +127,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+# Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 
 
